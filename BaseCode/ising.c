b9b9c26dac7ee720a0b0f7779f9c216865c59414
André Luiz Vasconcelos Ferreira <andreluizvf123@gmail.com>
2025-08-11 11:20:32 -0300

    Adiciona parametros ao script BaseCode

--- BaseCode/ising.c
+++ BaseCode/ising.c
@@ -2,18 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define N 32
-#define Temperatura 0.5
+#include <string.h>
+//#define N 32
+//#define Temperatura 10
 //#define DEBUG
 
 typedef struct{
-  int spins[N][N];
+  int **spins;
   float energy;
   float spin_sum;
   float J;
 }Ising;
 
-void create_spin_lattice(Ising *model, int proportion){
+int ** allocate_spins(int N){
+  int **matrix = malloc(N * sizeof(int *));
+  for(int i=0; i<N; i++){
+    matrix[i] = malloc(N * sizeof(int));
+  }
+  return matrix;
+}
+
+void free_spins(int **matrix, int N){
+  for(int i=0; i<N; i++){
+    free(matrix[i]);
+  }
+  free(matrix);
+}
+
+
+void create_spin_lattice(Ising *model, int proportion, int N){
   for(int i=0; i<N; i++){
     for(int j=0; j<N; j++){
       float random = (rand() % 1000) + 1;
@@ -25,14 +42,14 @@ void create_spin_lattice(Ising *model, int proportion){
   }
 }
 
-void calculate_spin_sum(Ising *model){
+void calculate_spin_sum(Ising *model, int N){
  model->spin_sum = 0;
  for(int i=0; i<N; i++)
   for(int j=0; j<N; j++)
     model->spin_sum+=model->spins[i][j];
 }
 
-void calculate_hamiltonian(Ising *model){
+void calculate_hamiltonian(Ising *model, int N){
   model->energy = 0;
   for(int i=0; i<N; i++){
     for(int j=0; j<N; j++){
@@ -50,7 +67,7 @@ void calculate_hamiltonian(Ising *model){
   model->energy*=-model->J;
 }
 
-void isingStep(Ising *X_t, float Temp){
+void isingStep(Ising *X_t, float Temp, int N){
     float dE=0, dEy=0, dEx=0;
  
     //Rotate random spin
@@ -104,7 +121,7 @@ void isingStep(Ising *X_t, float Temp){
     }
 }
 
-void print_model(Ising *X){
+void print_model(Ising *X, int N){
   for(int j=0; j<N; j++){
     for(int i=0; i<N; i++){
       if(X->spins[i][j] == -1)
@@ -116,28 +133,61 @@ void print_model(Ising *X){
   }
 }
 
-int main() {
+int main(int argc, char **argv) {
+
+    int proportion = 750;
+    unsigned int steps = 1000000;
+    int N = 32;
+    float Temperatura = 5;
+
     clock_t t,t_total;
     t_total = clock();
     srand(time(NULL));
-    unsigned int executions = 1000000;
     FILE *statistics;
     
     //Prepara o modelo
     Ising X;
     X.J = 1;
     
+    for (int i = 0; i < argc; i++) {
+      printf("Argumento [%d]: %s\n", i, argv[i]);
+  
+      if(strcmp(argv[i], "-P") == 0){
+        proportion = atoi(argv[i+1]);
+      }  
+
+      if(strcmp(argv[i], "-steps") == 0){
+        steps = atoi(argv[i+1]);
+      }      
+      
+      if(strcmp(argv[i], "-N") == 0){
+        N = atoi(argv[i+1]);
+      }
+
+      if(strcmp(argv[i], "-J") == 0){
+        X.J = atoi(argv[i+1]);
+      }
+
+      if(strcmp(argv[i], "-T") == 0){
+        Temperatura = atof(argv[i+1]);
+      }
+    }
+
+    X.spins = allocate_spins(N);
+    
+
     t = clock();
-    create_spin_lattice(&X,750);
+    create_spin_lattice(&X,proportion,N);
     t = clock() - t;
     printf("Tempo create_lattice:%f\n",(float)t/CLOCKS_PER_SEC);
     X.energy = 0;
     X.spin_sum = 0;
-    calculate_hamiltonian(&X);
-    calculate_spin_sum(&X);
+
+    calculate_hamiltonian(&X,N);
+    calculate_spin_sum(&X,N);
     #ifdef DEBUG
     printf("X\nenergy: %f\n",X.energy);
-    //print_model(&X);
+    //print_model(&X,N);
     #endif
     
     //Executa o simulador
@@ -146,8 +196,8 @@ int main() {
       printf("Can't open file");
     }else{
       t = clock();
-      for(int i=0; i<executions; i++){
-        isingStep(&X,Temperatura);
+      for(int i=0; i<steps; i++){
+        isingStep(&X,Temperatura,N);
         //Escreve no documento
         char mensagem[50];
         sprintf(mensagem, "%d,%.5f,%.3f\n", i, X.energy, X.spin_sum/(N*N));
@@ -159,8 +209,11 @@ int main() {
     fclose(statistics);
     #ifdef DEBUG
     printf("X\nenergy: %f\n",X.energy);
-    //print_model(&X);
+    //print_model(&X,N);
     #endif
+
+    free_spins(X.spins,N);
+
     t_total = clock() - t_total;
     printf("Tempo total:%f\n",(float)t_total/CLOCKS_PER_SEC);
     return 0;
